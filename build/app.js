12c8e034a742554b2bc1fc841715cf8f8e357f85
Kooku <koomg9599@gmail.com>
2020-01-18 19:52:33 +0900

    etc: gitignore 설정, 의존성 모듈 설치

--- build/app.js
+++ build/app.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const logger_1 = require("./logger");
 function runServer() {
     const app = express();
     app.listen(5000, () => {
-        console.log("start server");
-        console.log(process.env.NODE_ENV);
+        console.log('start server');
+        logger_1.default.info('Server app listening on port 5000!');
     });
 }
 runServer();
