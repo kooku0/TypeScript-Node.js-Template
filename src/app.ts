fbb4e6ba3268ba368baafc93c70d02ae6e390c63
Kooku <koomg9599@gmail.com>
2020-01-18 19:47:38 +0900

    feats: shutdown code 추가(graceful)

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,38 @@
 import * as express from 'express'
+import * as http from 'http'
 import logger from './logger'
 
-function runServer() {
+// Shutdown codes
+const errShutdown = async (server: http.Server, err?: Error) => {
+  logger.error(`Stopping server with error: ${err}`)
+  await server.close()
+  process.exit(1)
+}
+
+const graceShutdown = (signal: string) => {
+  logger.info(`Stopping server with signal: ${signal}`)
+  process.exit(0)
+}
+
+async function runServer() {
   const app = express()
-  app.listen(5000, () => {
-    console.log('start server')
-    console.log(process.env.NODE_ENV)
+  const server = app.listen(5000, () => {
+    logger.info('Server app listening on port 5000!')
   })
+  try {
+  } catch (e) {
+    errShutdown(server, e)
+    throw e
+  }
 }
 runServer()
+  .then(() => {
+    logger.info('run succesfully')
+  })
+  .catch((ex: Error) => {
+    logger.error('Unable run:', ex)
+  })
+
+// Graceful shutdown - SIGINT handling
+process.on('SIGINT', () => graceShutdown('SIGINT'))
+process.on('SIGTERM', () => graceShutdown('SIGTERM'))
